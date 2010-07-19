4fa578cc1a859654f9735ea1bd9f9c77875cd143
S.Çağlar Onur <caglar@cs.princeton.edu>
2010-07-19 15:09:03 -0400

    add helpers

--- func/minion/modules/nm/slicetagmanager.py
+++ func/minion/modules/nm/slicetagmanager.py
@@ -26,8 +26,42 @@ class SliceTagManager(func_module.FuncModule):
     api_version = "0.0.1"
     description = "SliceTagManager"
 
+    def __readFromFile__(self, filename):
+        if os.path.exists(filename):
+            f = open(filename, "r")
+            fcntl.flock(f.fileno(), fcntl.LOCK_SH)
+            filecontent = f.readlines()
+            fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+            f.close()
+        else:
+            filecontent = []
+
+        return filecontent
+
+    def __writeToFile__(self, filename, filecontent, sorted = True):
+        f = open(filename,"w")
+        fcntl.flock(f.fileno(), fcntl.LOCK_EX)
+
+        sortedfilecontent = filecontent
+        if sorted == True:
+            sortedfilecontent = list(set(filecontent))
+            sortedfilecontent.sort()
+
+        for i in sortedfilecontent:
+            f.write("%s" % i)
+        fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+        f.close()
+
     def AddSliceTag(self, slice, tag, value):
-        pass
+        # Sanity check
+        try:
+            vserver_instance = vserver.VServer(slice)
+        except vserver.NoSuchVServer:
+            return False
     
     def DeleteSliceTag(self, slice, tag, value):
-        pass
+         # Sanity check
+        try:
+            vserver_instance = vserver.VServer(slice)
+        except vserver.NoSuchVServer:
+            return False
